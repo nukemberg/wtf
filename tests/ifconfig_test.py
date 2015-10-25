b50e55d786f1e1fd7ffc47c007f12fe6c1ea5fae
Avishai Ish-Shalom <avishai@fewbytes.com>
2015-10-26 01:34:07 +0200

    support python 2.6 syntax in test

--- tests/ifconfig_test.py
+++ tests/ifconfig_test.py
@@ -3,6 +3,8 @@ from mock import patch
 import psutil
 from psutil._common import snicstats, snetio
 from wtf.plugins.ifconfig import Ifconfig
+import contextlib
+
 
 class IfconfigPluginTest(unittest.TestCase):
 
@@ -10,8 +12,9 @@ class IfconfigPluginTest(unittest.TestCase):
         self._plugin = Ifconfig({})
 
     def testDownIfaces(self):
-        with patch.object(psutil, 'net_io_counters', return_value={'lo': {}}), \
-                patch.object(psutil, 'net_if_stats', return_value={'eth0': snicstats(False, 0, 0, 1500)}):
+        with contextlib.nested( 
+            patch.object(psutil, 'net_io_counters', return_value={'lo': {}}),
+            patch.object(psutil, 'net_if_stats', return_value={'eth0': snicstats(False, 0, 0, 1500)})):
             output = self._plugin.run()
             self.assertEqual(output['problem'], 'The following interfaces are down: eth0')
 
@@ -23,14 +26,16 @@ class IfconfigPluginTest(unittest.TestCase):
             snetio(122312, 2312312, 3432, 34324, 0, 0, 0, 1)
         ]:
 
-            with patch.object(psutil, 'net_io_counters', return_value={'eth0': test_snetio}), \
-                    patch.object(psutil, 'net_if_stats', return_value={'eth0': snicstats(True, 0, 0, 1500)}):
+            with contextlib.nested(
+                patch.object(psutil, 'net_io_counters', return_value={'eth0': test_snetio}),
+                patch.object(psutil, 'net_if_stats', return_value={'eth0': snicstats(True, 0, 0, 1500)})):
                 output = self._plugin.run()
                 self.assertRegexpMatches(output['problem'], 'issues.*eth0')
 
     def testIfaceIgnored(self):
         plugin = Ifconfig({'ignored': ['eth0']})
-        with patch.object(psutil, 'net_io_counters', return_value={'eth0': snetio(1122, 233, 333, 333, 1, 1, 1, 1)}), \
-                patch.object(psutil, 'net_if_stats', return_value={'eth0': snicstats(False, 0, 0, 1500)}):
+        with contextlib.nested(
+            patch.object(psutil, 'net_io_counters', return_value={'eth0': snetio(1122, 233, 333, 333, 1, 1, 1, 1)}),
+            patch.object(psutil, 'net_if_stats', return_value={'eth0': snicstats(False, 0, 0, 1500)})):
             output = plugin.run()
             self.assertFalse(output['problem'])
